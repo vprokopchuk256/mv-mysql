379c0c016e0525469ff8501cd43df3c26cd0ebd0
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-11 10:02:23 +0200

    exclusion in trigger validation builder

---
+++ lib/mv/mysql/validation/builder/trigger/exclusion.rb
@@ -0,0 +1,24 @@
+module Mv
+  module Mysql
+    module Validation
+      module Builder
+        module Trigger
+          class Exclusion < Mv::Core::Validation::Builder::Exclusion
+            protected
+            
+            def column_reference
+              "NEW.#{super}"
+            end 
+
+            def db_value value
+              return "TIMESTAMP('#{value.strftime('%Y-%m-%d %H:%M:%S')}')" if value.is_a?(DateTime)
+              return "TIME('#{value.strftime('%Y-%m-%d %H:%M:%S')}')" if value.is_a?(Time)
+              return "DATE('#{value.strftime('%Y-%m-%d')}')" if value.is_a?(Date)
+              super
+            end
+          end
+        end
+      end
+    end
+  end
+end
