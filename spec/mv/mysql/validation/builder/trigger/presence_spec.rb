132c31b6abc073806dd31609846ddb8af1ecdd25
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-01-11 11:47:11 +0200

    presence in trigger validation builder

---
+++ spec/mv/mysql/validation/builder/trigger/presence_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+require 'mv/mysql/validation/builder/trigger/presence'
+
+describe Mv::Mysql::Validation::Builder::Trigger::Presence do
+  def presence(opts = {})
+    Mv::Core::Validation::Presence.new(:table_name, 
+                                       :column_name,
+                                        { message: 'some error message' }.merge(opts))
+  end
+
+  describe "#conditions" do
+    subject { described_class.new(presence(opts)).conditions }
+
+    describe "by default" do
+      let(:opts) { {} }
+       
+      it { is_expected.to eq([{
+        statement: "NEW.column_name IS NOT NULL AND LENGTH(TRIM(NEW.column_name)) > 0", 
+        message: 'some error message'
+      }]) }
+    end 
+  end
+end
